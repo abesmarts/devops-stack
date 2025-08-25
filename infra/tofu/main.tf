0fafc5797e8046230138c5ca6a8f88d70dc580a3
abner espinoza <abespinoza155@gmail.com>
2025-08-25 10:48:59 -0400

    removing variables to test

--- infra/tofu/main.tf
+++ infra/tofu/main.tf
@@ -9,46 +9,7 @@ terraform {
   }
 }
 
-# ------------------------
-# Variables
-# ------------------------
-variable "name" {
-  type    = string
-  default = "semproj"
-}
 
-variable "gcp_project" {
-  type = string
-}
-
-variable "gcp_region" {
-  type    = string
-  default = "us-central1"
-}
-
-variable "gcp_zone" {
-  type    = string
-  default = "us-central1-c"
-}
-
-variable "machine_type" {
-  type    = string
-  default = "e2-medium"
-}
-
-variable "ubuntu_image" {
-  type    = string
-  default = "ubuntu-os-cloud/ubuntu-2404-lts"
-}
-
-variable "ssh_public_key" {
-  type = string
-}
-
-variable "ssh_source_ranges" {
-  type    = list(string)
-  default = ["0.0.0.0/0"]
-}
 
 # ------------------------
 # Provider
